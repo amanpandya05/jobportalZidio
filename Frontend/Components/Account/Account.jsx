cfa3bf6582ae5428c7f5bc5bf179f8d87a81cdb2
AyeshaRehan2619 <ayesharehan680@gmail.com>
2024-09-03 20:58:31 +0530

    Add files via upload

---
+++ Frontend/Components/Account/Account.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Account() {
+  return (
+    <>
+     
+
+    <div className="account-box">
+    <h1>My Profile</h1>
+   <h3>Your Name : <span className='purple'>Rehan Ahmad</span></h3>
+   <h3>Your Position : <span className='purple'>Senior Software Developer</span></h3>
+   <h3>Your Email : <span className='purple'>Rehan123@gmail.com </span></h3>
+   <h3>Your Phone Number : <span className='purple'>9987654323</span></h3>
+  <h3>Your Address : <span className='purple'>8th Phase Gomti Nagar , Lucknow</span></h3>
+  <h3>Skills : <span className='purple'>React.js , MERN Stack , Java Programming , Typescript , Graphic Design</span></h3>
+  <h3>Work Experience : <span className='purple'>5+ Years in MERN Stack , 5 + Years in Graphic Design</span></h3>
+  <h3>Education : <span className='purple'>B.tech From IIT Kanpur</span></h3>
+  <h3>Licenses & Certifications : <span className='purple'>Certified MERN Stack Developer , Certified Graphic Designer</span></h3>
+  <h3>Resume :</h3>
+  <h3>Github :</h3>
+
+    </div>
+    </>
+  )
+}
+
+export default Account
---
+++ Frontend/Components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+
+import React from "react"
+//import './App.css'
+
+function Navbar() {
+    return (
+        <div className="navbar">
+        <div className="container">
+        
+       <ul>
+         <li>Job Hunt</li>
+          <li> Home</li>
+          <li>Jobs</li>
+          <li>Browse</li>
+          <li>Register</li>
+          <li>Account</li>
+        </ul>
+        </div>
+        </div>
+    )
+}
+
+export default Navbar
+
+
+      
